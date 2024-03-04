fc934fce4156cc3769d30a749a9811e7eea66804
Raymond Jiang <raymondjiang0917@gmail.com>
2024-03-03 23:14:48 -0800

    added commments

--- ACSL 2024 Contest 2 Book Cipher/ACSL 2024 Contest 2.py
+++ ACSL 2024 Contest 2 Book Cipher/ACSL 2024 Contest 2.py
@@ -3,36 +3,37 @@ text = "Four score and seven years ago our fathers brought forth on this contine
 
 message = "The #1 speech of all time was less than 8 minutes long!"
 
-book = re.split(r'\!  |\?  |\.\  ', text)
-book = [re.split(r'[^a-zA-Z0-9]+', s) for s in book]
+book = re.split(r'\!  |\?  |\.\  ', text) #split text into array of sentences based on !, ?, .
+book = [re.split(r'[^a-zA-Z0-9]+', s) for s in book] #split each sentence into array of words based on all symbols
 print(book)
-def find_occ(text, letter, n):
-    current_occ = 0
-    for sentence in range(len(book)):
+
+def find_occ(text, letter, n): #finds the s.w.c value based on text, letter, and occurrence #
+    current_occ = 0 #used to check if occurrence matches specficed one
+    for sentence in range(len(book)): #only used len() because s.w.c requires numbers
         for word in range(len(book[sentence])):
             for l in range(len(book[sentence][word])):
-                let = book[sentence][word][l]
-                if let == letter:
-                    current_occ += 1
-                    if current_occ == n:
-                        return str(sentence + 1) + "." + str(word + 1) + "." + str(l + 1)
-    return find_occ(text, letter, max(1, n // 2))
+                let = book[sentence][word][l] #let is now equal to a singular letter
+                if let == letter: #checks if let is equal to the wanted number
+                    current_occ += 1 #if so add occurrence # by 1
+                    if current_occ == n: #if occurrence # is equal to the wanted number
+                        return str(sentence + 1) + "." + str(word + 1) + "." + str(l + 1) #return s.w.c format (add one since index starts at 0)
+    return find_occ(text, letter, max(1, n // 2)) #if the current occurrence # is too high, try again recursively with //2
 
 encode = ""
 char = 0
 sym = True
-for i in range(len(message)):
-    if message[i] == " ":
+for i in range(len(message)): #iterate through message
+    if message[i] == " ": #checks if its a space, if so, add "_" to encode
         encode += "_"
-        sym = True
-    elif re.match(r'^[^a-zA-Z0-9]+$', message[i]):
-        encode += message[i]
+        sym = True #this is for the spacing issue (when to put a space), probably not the best way to solve but
+    elif re.match(r'^[^a-zA-Z0-9]+$', message[i]): #checks if the selected letter is a symbol
+        encode += message[i] #if so, add symbol to encode
         sym = True
     else:
-        if not sym:
+        if not sym: #if there wasnt a symbol/space before it, add a space
             encode += " "
-        encode += find_occ(text, message[i], char + 1)
-        char += 1
-        sym = False
+        encode += find_occ(text, message[i], char + 1) #append the output of s.w.c to encode
+        char += 1 #since symbols do not count as characters, add one only if its a character (non symbols)
+        sym = False #reset symbol variable
         
 print(encode)
